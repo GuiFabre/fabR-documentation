301263f21acce4770020540d6f186637adc5118d
guifabre <guijoseph.fabre@gmail.com>
2023-03-06 17:38:31 +0000

    update documentation

--- update_files.R
+++ update_files.R
@@ -1,10 +1,24 @@
 library(harmonizRv3)
 library(fs)
-# usethis::use_pkgdown()
+tinytex::tlmgr_install("makeindex")
 
+usethis::use_pkgdown()
+usethis::use_github_action_check_standard()
+usethis::use_readme_rmd()
+
+fs::dir_delete("/staff/gfabre/fabR/docs")
+fs::dir_delete("/staff/gfabre/fabR-documentation/docs")
+
+
+devtools::build_readme()
+# usethis::use_package_doc()
+# usethis::use_vignette("fabR-vignette")
 devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
-pkgdown::build_site()
+devtools::check()
+devtools::build()
 
+
+pkgdown::build_site()
 fs::dir_copy(
   "/staff/gfabre/fabR/docs",
   "/staff/gfabre/fabR-documentation/docs",
