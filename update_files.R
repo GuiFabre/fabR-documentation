9d82c0d289a735913c5b170eefc59892485ba7e6
guifabre <guijoseph.fabre@gmail.com>
2023-03-07 20:22:07 +0000

    update documentation

--- update_files.R
+++ update_files.R
@@ -1,23 +1,32 @@
 library(harmonizRv3)
 library(fs)
-tinytex::tlmgr_install("makeindex")
-
+library(goodpractice)
 usethis::use_pkgdown()
 usethis::use_github_action_check_standard()
 usethis::use_readme_rmd()
 
-fs::dir_delete("/staff/gfabre/fabR/docs")
-fs::dir_delete("/staff/gfabre/fabR-documentation/docs")
 
+# https://devguide.ropensci.org/building.html
+# https://r-pkgs.org/release.html#sec-release-process
+# https://kalimu.github.io/post/checklist-for-r-package-submission-to-cran/
 
-devtools::build_readme()
 # usethis::use_package_doc()
 # usethis::use_vignette("fabR-vignette")
+# usethis::use_cran_comments()
 devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
+devtools::build_readme()
+spelling::spell_check_package(".")
+devtools::build_manual()
 devtools::check()
-devtools::build()
+goodpractice::gp()
+devtools::build(manual = TRUE)
+# push to git
+devtools::submit_cran()
+# push to git
 
 
+fs::dir_delete("/staff/gfabre/fabR/docs")
+fs::dir_delete("/staff/gfabre/fabR-documentation/docs")
 pkgdown::build_site()
 fs::dir_copy(
   "/staff/gfabre/fabR/docs",
